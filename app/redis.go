440474f6aa697ecab2870b0233df8077b5f0f74a
Digital39999 <github@crni.xyz>
2024-09-14 11:59:04 +0200

    Remove unused code in createRedisClient function

--- app/redis.go
+++ app/redis.go
@@ -23,12 +23,5 @@ func createRedisClient(redisURL string) (*redis.Client, error) {
 	}
 
 	fmt.Println("Connected to Redis.")
-
-	_, err = client.ConfigSet(ctx, "notify-keyspace-events", "Ex").Result()
-	if err != nil {
-		fmt.Println("Failed to set notify-keyspace-events:", err)
-		return nil, err
-	}
-
 	return client, nil
 }
